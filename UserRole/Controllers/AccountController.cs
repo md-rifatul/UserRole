6a590967f081d2e6c6886ba3aeb9018ff9076495
Rifatul <rifatul.ism@gmail.com>
2026-01-09 23:55:54 +0600

    Inplement dependency injection in AccountController

--- UserRole/Controllers/AccountController.cs
+++ UserRole/Controllers/AccountController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserRole.Models;
 
 namespace UserRole.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly SignInManager<Users> _signInManager;
+        private readonly UserManager<Users> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager, RoleManager<IdentityRole> roleManager)
         {
-            return View();
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
+
         public IActionResult Login()
         {
             return View();
