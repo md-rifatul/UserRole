caed487f9cba47dff2ed8284a20d191a4b401adb
Rifatul <rifatul.ism@gmail.com>
2026-01-10 00:18:19 +0600

    Implement login

--- UserRole/Controllers/AccountController.cs
+++ UserRole/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UserRole.Models;
+using UserRole.ViewModels;
 
 namespace UserRole.Controllers
 {
@@ -20,6 +21,26 @@ namespace UserRole.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid Login Attempt.");
+            return View(model);
         }
     }
 }
